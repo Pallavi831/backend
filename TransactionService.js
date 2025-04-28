af3df974fc0e75d4ab35a98441754dbb399f2921
Pallavi C <info@joindevops.com>
2025-04-28 22:45:01 +0530

    backend

--- TransactionService.js
+++ TransactionService.js
@@ -6,6 +6,7 @@ const con = mysql.createConnection({
     user: process.env.DB_USER || dbcreds.DB_USER,
     password: process.env.DB_PWD || dbcreds.DB_PWD,
     database: process.env.DB_DATABASE || dbcreds.DB_DATABASE
+
 });
 
 function addTransaction(amount,desc){
@@ -35,6 +36,7 @@ function findTransactionById(id,callback){
     }) 
 }
 
+
 function deleteAllTransactions(callback){
     var mysql = "DELETE FROM transactions";
     con.query(mysql, function(err,result){
@@ -60,4 +62,15 @@ module.exports = {
     deleteAllTransactions,
     deleteTransactionById
 };
+
+// Create a connection pool for better performance and scalability
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || dbcreds.DB_HOST,
+    user: process.env.DB_USER || dbcreds.DB_USER,
+    password: process.env.DB_PWD || dbcreds.DB_PWD,
+    database: process.env.DB_DATABASE || dbcreds.DB_DATABASE,
+    waitForConnections: true, // Allows the pool to wait for available connections
+    connectionLimit: 10, // Number of connections allowed in the pool
+    queueLimit: 0 // No limit for queued connections
+});
 
